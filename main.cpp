6c90766ae3dec6699133cb73c373eca97ec031d6
yo <yo>
2022-06-04 18:32:43 +0300

    Cosmetics

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@ using namespace std;
 int main() {
     int a, b;
     cout << "Enter A: ";
+
     cin >> a;
+
     cout << "Enter B: ";
+
     cin >> b;
+
     cout << "A + B = " << a + b << '\n';
+
     cout << "A - B = " << a - b << '\n';
+
     cout << "A * B = " << a * b << '\n';
 }
