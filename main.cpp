19ddf5a4fe8b5c3b2bb350de18a6c7be961245d4
yo <yo>
2022-06-04 18:22:24 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter A: ";
+    cin >> a;
+    cout << "Enter B: ";
+    cin >> b;
+    cout << "A + B = " << a + b << '\n';
+    cout << "A - B = " << a - b << '\n';
+    cout << "A * B = " << a * b << '\n';
+}
